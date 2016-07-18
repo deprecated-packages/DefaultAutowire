23978d2901c0e706924e796e59faca5b02a74b42
TomasVotruba <tomas.vot@gmail.com>
2016-07-18 12:15:55 +0200

    improve readme and fix cs

--- README.md
+++ README.md
@@ -62,21 +62,36 @@ class AppKernel extends Kernel
 }
 ```
 
+
 And that's it!
 
 
 # Features
 
-# Multiple service of one type? Set preferred one
+## Multiple service of one type? Set preferred one
+
+In case you have multiple connections, entity managers or decorated service,
+**you can set default preferred service (name) for each type (class or interface)**.
+
+To make it easier for you, there are few default values. You can change them or add new ones.
 
 ```yaml
-
-
+# app/config/cofing.yml
+symplify_default_autowire:
+    autowire_types:
+        Doctrine\ORM\EntityManager: 'doctrine.orm.default_entity_manager'
+        Doctrine\ORM\EntityManagerInterface: 'doctrine.orm.default_entity_manager'
+        Doctrine\Portability\Connection: 'database_connection'
+        Symfony\Component\EventDispatcher\EventDispatcher: 'event_dispatcher'
+        Symfony\Component\EventDispatcher\EventDispatcherInterface: 'event_dispatcher'
+        Symfony\Component\Translation\TranslatorInterface: 'translator'
+```
 
 
 # Testing
 
 ```bash
+$ vendor/bin/symplify-cs check src tests
 $ vendor/bin/phpunit
 ```
 
--- tests/Config/Definition/ConfigurationResolverTest.php
+++ tests/Config/Definition/ConfigurationResolverTest.php
@@ -5,7 +5,6 @@ namespace Symplify\DefaultAutowire\Tests\Config\Definition;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symplify\DefaultAutowire\Config\Definition\ConfigurationResolver;
-use Symplify\DefaultAutowire\DependencyInjection\Extension\SymplifyDefaultAutowireContainerExtension;
 use Symplify\DefaultAutowire\SymplifyDefaultAutowireBundle;
 
 final class ConfigurationResolverTest extends TestCase
@@ -20,21 +19,22 @@ final class ConfigurationResolverTest extends TestCase
         $this->configurationResolver = new ConfigurationResolver();
     }
 
-//    public function testResolveDefaults()
-//    {
-//        $resolvedConfiguration = $this->configurationResolver->resolveFromContainerBuilder(new ContainerBuilder());
-//
-//        $this->assertSame([
-//            'autowire_types' => [
-//                'Doctrine\ORM\EntityManager' => 'doctrine.orm.default_entity_manager',
-//                'Doctrine\ORM\EntityManagerInterface' => 'doctrine.orm.default_entity_manager',
-//                'Doctrine\Portability\Connection' => 'database_connection',
-//                'Symfony\Component\EventDispatcher\EventDispatcher' => 'event_dispatcher',
-//                'Symfony\Component\EventDispatcher\EventDispatcherInterface' => 'event_dispatcher',
-//                'Symfony\Component\Translation\TranslatorInterface' => 'translator',
-//            ]
-//        ], $resolvedConfiguration);
-//    }
+    public function testResolveDefaults()
+    {
+        $resolvedConfiguration = $this->configurationResolver->resolveFromContainerBuilder(new ContainerBuilder());
+
+        $this->assertCount(6, $resolvedConfiguration['autowire_types']);
+        $this->assertSame([
+            'autowire_types' => [
+                'Doctrine\ORM\EntityManager' => 'doctrine.orm.default_entity_manager',
+                'Doctrine\ORM\EntityManagerInterface' => 'doctrine.orm.default_entity_manager',
+                'Doctrine\Portability\Connection' => 'database_connection',
+                'Symfony\Component\EventDispatcher\EventDispatcher' => 'event_dispatcher',
+                'Symfony\Component\EventDispatcher\EventDispatcherInterface' => 'event_dispatcher',
+                'Symfony\Component\Translation\TranslatorInterface' => 'translator',
+            ],
+        ], $resolvedConfiguration);
+    }
 
     public function testOverrideDefaults()
     {
@@ -42,15 +42,35 @@ final class ConfigurationResolverTest extends TestCase
 
         $containerBuilder->prependExtensionConfig(SymplifyDefaultAutowireBundle::ALIAS, [
             'autowire_types' => [
-                'Doctrine\ORM\EntityManager' => 'other_entity_manager'
-            ]
+                'Doctrine\ORM\EntityManager' => 'other_entity_manager',
+            ],
         ]);
 
         $resolvedConfiguration = $this->configurationResolver->resolveFromContainerBuilder(
             $containerBuilder
         );
 
-        $this->assertSame('other_entity_manager', $resolvedConfiguration['autowire_types']['Doctrine\ORM\EntityManager']);
-        $this->assertCount(6, $resolvedConfiguration['autowire_types']);
+        $autowireTypes = $resolvedConfiguration['autowire_types'];
+        $this->assertCount(6, $autowireTypes);
+        $this->assertSame('other_entity_manager', $autowireTypes['Doctrine\ORM\EntityManager']);
+    }
+
+    public function testAddNewValues()
+    {
+        $containerBuilder = new ContainerBuilder();
+
+        $containerBuilder->prependExtensionConfig(SymplifyDefaultAutowireBundle::ALIAS, [
+            'autowire_types' => [
+                'SomeInterface' => 'some_service',
+            ],
+        ]);
+
+        $resolvedConfiguration = $this->configurationResolver->resolveFromContainerBuilder(
+            $containerBuilder
+        );
+
+        $autowireTypes = $resolvedConfiguration['autowire_types'];
+        $this->assertCount(7, $autowireTypes);
+        $this->assertSame('some_service', $autowireTypes['SomeInterface']);
     }
 }
