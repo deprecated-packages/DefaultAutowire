cdb645e658f4f303a616b757c909617d6e1989e5
TomasVotruba <tomas.vot@gmail.com>
2016-10-26 21:33:26 +0200

    rename /src to more propper name /packages [closes #3]

---
+++ tests/AppKernel.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Symplify\DefaultAutowire\Tests;
+
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\HttpKernel\Kernel;
+use Symplify\DefaultAutowire\SymplifyDefaultAutowireBundle;
+
+final class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return [
+            new SymplifyDefaultAutowireBundle(),
+            new DoctrineBundle(),
+        ];
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__ . '/Resources/config/config.yml');
+    }
+}
